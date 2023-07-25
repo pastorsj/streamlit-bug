6e13a7cc76be0af8ce94961c988e913f8983d068
Sam Pastoriza <pastorsj@users.noreply.github.com>
2023-07-25 10:27:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,21 @@ import pandas as pd
 
 
 def main():
-    st.file_uploader(
-        "Upload a file",
-        type=["xlsx", "xls"],
-        key="file",
-    )
-
-    if st.session_state.file is not None:
-        if st.button(
-            "Run",
-            type="primary",
-        ):
-            start_time = time.time()
-            st.progress(0, "Testing...")
-            file_df = pd.read_excel(st.session_state.file)
-            for idx, row in file_df.iterrows():
-                time.sleep(15)
-                end_time = time.time()
-                st.write(row)
-                st.progress(
-                    idx / len(file_df), f"Total Time: {round(end_time - start_time)} seconds..."
-                )
+    if st.button(
+        "Run",
+        type="primary",
+    ):
+        start_time = time.time()
+        st.progress(0, "Testing...")
+        idx = 0
+        while idx < 100:
+            time.sleep(15)
+            end_time = time.time()
+            st.progress(
+                (idx + 1) / 100,
+                f"Total Time: {round(end_time - start_time)} seconds...",
+            )
+            idx += 1
 
 
 if __name__ == "__main__":
